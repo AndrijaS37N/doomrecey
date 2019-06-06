80d710b9029ee49b9784e3fca6e5570edd58864e
Andrija <AndrijaS37N@users.noreply.github.com>
2019-06-06 13:54:54 +0200

    Adding CMake.

---
+++ build/CMakeFiles/doomrecey++.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/andrijadjuric/Sites/GitHub/doomrecey/animal.cpp" "/Users/andrijadjuric/Sites/GitHub/doomrecey/build/CMakeFiles/doomrecey++.dir/animal.cpp.o"
+  "/Users/andrijadjuric/Sites/GitHub/doomrecey/cat.cpp" "/Users/andrijadjuric/Sites/GitHub/doomrecey/build/CMakeFiles/doomrecey++.dir/cat.cpp.o"
+  "/Users/andrijadjuric/Sites/GitHub/doomrecey/dog.cpp" "/Users/andrijadjuric/Sites/GitHub/doomrecey/build/CMakeFiles/doomrecey++.dir/dog.cpp.o"
+  "/Users/andrijadjuric/Sites/GitHub/doomrecey/intermediate_io.cpp" "/Users/andrijadjuric/Sites/GitHub/doomrecey/build/CMakeFiles/doomrecey++.dir/intermediate_io.cpp.o"
+  "/Users/andrijadjuric/Sites/GitHub/doomrecey/main.cpp" "/Users/andrijadjuric/Sites/GitHub/doomrecey/build/CMakeFiles/doomrecey++.dir/main.cpp.o"
+  "/Users/andrijadjuric/Sites/GitHub/doomrecey/simple_io.cpp" "/Users/andrijadjuric/Sites/GitHub/doomrecey/build/CMakeFiles/doomrecey++.dir/simple_io.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/doomrecey++.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/doomrecey++.dir/main.cpp.o"
+  "CMakeFiles/doomrecey++.dir/simple_io.cpp.o"
+  "CMakeFiles/doomrecey++.dir/intermediate_io.cpp.o"
+  "CMakeFiles/doomrecey++.dir/animal.cpp.o"
+  "CMakeFiles/doomrecey++.dir/dog.cpp.o"
+  "CMakeFiles/doomrecey++.dir/cat.cpp.o"
+  "doomrecey++.pdb"
+  "doomrecey++"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/doomrecey++.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
