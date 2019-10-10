3ea68d766d0e3c4b5fc145706f4f612cd4e9e1e5
Andrija <AndrijaS37N@users.noreply.github.com>
2019-10-10 17:10:28 +0200

    Go on.

--- practice_part_three/part_three_linker.hpp
+++ practice_part_three/part_three_linker.hpp
@@ -3,153 +3,212 @@
 
 using namespace std;
 
-int addition(int *a, int *b)
-{
-    *a = 10;
-    return *a + *b;
-}
+// int addition(int *a, int *b)
+// {
+//     *a = 10;
+//     return *a + *b;
+// }
 
-int addition_ref(int &a, int &b)
-{
-    a = 0;
-    b = 1;
-    return a + b;
-}
+// int addition_ref(int &a, int &b)
+// {
+//     a = 0;
+//     b = 1;
+//     return a + b;
+// }
 
-void printArray(int arr[], int n)
-{
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-}
+// void printArray(int arr[], int n)
+// {
+//     for (int i = 0; i < n; i++)
+//         printf("%d ", arr[i]);
+// }
 
-class Entity
-{
-public:
-    Entity() { cout << "Entity made!" << endl; }
-    ~Entity() { cout << "Entity destroyed!" << endl; }
-    int m_a;
-    int m_b;
-    void func() { cout << "Entity func!" << endl; }
+// class Entity
+// {
+// public:
+//     Entity() {}
+//     // Entity(int a, float b) 
+//     // { 
+//     //     m_a = a;
+//     //     m_b = b;
+//     //     cout << "Entity made!" << endl; 
+//     // }
+//     ~Entity() { cout << "Entity destroyed!" << endl; }
+//     int m_a;
+//     float m_b;
+//     void func() 
+//     { 
+//         cout << "Entity func!" << endl; 
+//         cout << m_a << endl;
+//         cout << m_b << endl;
+//     }
+//     virtual void activate_func_private() { func_private(); }
 
-private:
-    void func_private() { cout << "Entity private func!" << endl; }
+// private:
+//     void func_private() { cout << "Entity private func!" << endl; }
+// };
+
+// class Player : public Entity
+// {
+// public:
+//     Player() { cout << "Player made!" << endl; }
+//     ~Player() { cout << "Player destroyed!" << endl; }
+//     int m_c;
+//     void activate_func_private() override { func_private_player(); }
+
+// private:
+//     void func_private_player() { cout << "Player private func!" << endl; }
+// };
+
+class C { 
+    int i; // 4
+    char c; // 1 - 1/4
+    char p; // 1 - 2/4
+}; // <=> 4 + 4 chunk blocks = 8 bytes
+
+class B {
+    C c; // 8
+    int x; // 4
+}; // <=> 12
+
+class A {
+    char c;
+    double d;
+    char h;
+    int i;
 };
 
-class Player : public Entity
-{
-public:
-    Player() { cout << "Player made!" << endl; }
-    ~Player() { cout << "Player destroyed!" << endl; }
-    int m_c;
-
-private:
-    void func_private() { cout << "Player private func!" << endl; }
+class A1 {
+    A a;
+    int x;
+    int q;
 };
+
+/*
+    The biggest data structure dictates the block size.
+*/
 
 void activate_part_three()
 {
-    Entity entity;
-    entity.func();
-    Player player;
-    player.func();
+    C c;
+    cout << sizeof(c) << endl;
+    B b;
+    cout << sizeof(b) << endl;
+    A a;
+    cout << sizeof(a) << endl;
+    A1 a1;
+    cout << sizeof(a1) << endl;
+    // Entity entity(5, 6.4f);
+    // Entity entity;
+    // entity.func();
+    // Player player;
+    // player.func();
 
-    int a = 4;
-    int b = 3;
-    cout << a << " " << b << endl;
-    cout << addition(&a, &b) << endl;
-    cout << a << " " << b << endl;
-    cout << addition_ref(a, b) << endl;
-    cout << a << " " << b << endl;
+    // entity.activate_func_private();
+    // player.activate_func_private();
 
-    int *c_array = (int *)calloc(10, sizeof(int));
-    cout << "C array stuff:" << endl;
-    cout << c_array << endl;        // memory address
-    cout << c_array + 1 << endl;    // memory address above + next block (sizeof(int) = 4)
-    cout << *c_array << endl;       // value of the first element
-    cout << *(c_array + 1) << endl; // value of the second element in the array
-    for (int i = 0; i < 10; i++)    // 10 loops
-    {
-        c_array[i] = 7; // set 0 values from calloc to 7s
-        cout << c_array[i] << " ";
-    }
-    cout << endl;
-    cout << "First element of the array size: " << sizeof(c_array[0]) << endl; // *c_array, int = 4
-    cout << "Array size: " << sizeof(c_array) << endl;                         // 64-bit computer => 8
-    cout << "Array pointer size: " << sizeof(*c_array) << endl;                // c_array[0], int = 4
+    // cout << "sizeof(entity): " << sizeof(entity) << endl;
+    // cout << "sizeof(player): " << sizeof(player) << endl;
 
-    cout << "Char array size input:" << endl;
-    int n;
-    cin >> n;
-    char *char_array = (char *)calloc(n, sizeof(char));
-    cout << (int)*char_array << endl;
-    // the below line is the same as (int *) &char_array[0] => the address of the first element
-    cout << "Char array print: " << (int *)char_array << endl; // must be casted to a int * because it's a memory address in hex
-    cout << sizeof char_array << endl;                         // sizeof memory address, 8 bytes
-    cout << sizeof *char_array << endl;                        // sizeof first element of the array, char = 1 byte
-    for (int i = 0; i < n; i++)
-    {
-        cout << (int)char_array[i] << " ";
-        char_array[i] = 'f';               // will be
-        cout << (int)char_array[i] << " "; // 102
-    }
-    cout << endl;
+    // cout << sizeof(short) << endl;
+    // cout << sizeof(long) << endl;
 
-    // realloc example
-    printf("Input m: ");
-    int m;
-    scanf("%i", &m);
-    char *string_pointer = (char *)malloc(sizeof(int) * m);
-    cout << "Strlen: " << strlen(string_pointer) << endl;
-    strcpy(string_pointer, "Keyush");
-    cout << "Strlen: " << strlen(string_pointer) << endl;
-    printf("String = %s, address = %p\n", string_pointer, string_pointer);
-    // reallocating memory
-    string_pointer = (char *)realloc(string_pointer, sizeof(int) * m * 2);
-    strcat(string_pointer, ": 'Ruf, ruf!'");
-    cout << "Strlen: " << strlen(string_pointer) << endl;
-    printf("String = %s, address = %p\n", string_pointer, string_pointer);
-    cout << "string_pointer[strlen(string_pointer) - 1] = " << string_pointer[strlen(string_pointer) - 1] << endl;
-    for (int i = 0; i < 20; i++)
-    {
-        printf("%c", string_pointer[i]);
-        if (string_pointer[i] == '\0')
-        {
-            printf("\nEnd of string!\n");
-            printf("%c", string_pointer[i]);
-            break;
-        }
-    }
+    // int a = 4;
+    // int b = 3;
+    // cout << a << " " << b << endl;
+    // cout << addition(&a, &b) << endl;
+    // cout << a << " " << b << endl;
+    // cout << addition_ref(a, b) << endl;
+    // cout << a << " " << b << endl;
 
-    // examples of using const
-    const int *const_int;                               // declares that const_int is a variable pointer to a constant integer
-    int const *const_int_b;                             // is an alternative syntax which does the same
-    int *const const_pointer = nullptr;                 // declares that const_pointer is constant pointer to a variable integer
-    int const *const const_int_const_pointer = nullptr; // declares that const_int_const_pointer is constant pointer to a constant integer
+    // int *c_array = (int *)calloc(10, sizeof(int));
+    // cout << "C array stuff:" << endl;
+    // cout << c_array << endl;        // memory address
+    // cout << c_array + 1 << endl;    // memory address above + next block (sizeof(int) = 4)
+    // cout << *c_array << endl;       // value of the first element
+    // cout << *(c_array + 1) << endl; // value of the second element in the array
+    // for (int i = 0; i < 10; i++)    // 10 loops
+    // {
+    //     c_array[i] = 7; // set 0 values from calloc to 7s
+    //     cout << c_array[i] << " ";
+    // }
+    // cout << endl;
+    // cout << "First element of the array size: " << sizeof(c_array[0]) << endl; // *c_array, int = 4
+    // cout << "Array size: " << sizeof(c_array) << endl;                         // 64-bit computer => 8
+    // cout << "Array pointer size: " << sizeof(*c_array) << endl;                // c_array[0], int = 4
 
-    /*
-        Basically ‘const’ applies to whatever is on its immediate left (other than if there is nothing there in which case it applies to whatever is its immediate right).
-    */
+    // cout << "Char array size input:" << endl;
+    // int n;
+    // cin >> n;
+    // char *char_array = (char *)calloc(n, sizeof(char));
+    // cout << (int)*char_array << endl;
+    // // the below line is the same as (int *) &char_array[0] => the address of the first element
+    // cout << "Char array print: " << (int *)char_array << endl; // must be casted to a int * because it's a memory address in hex
+    // cout << sizeof char_array << endl;                         // sizeof memory address, 8 bytes
+    // cout << sizeof *char_array << endl;                        // sizeof first element of the array, char = 1 byte
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << (int)char_array[i] << " ";
+    //     char_array[i] = 'f';               // will be
+    //     cout << (int)char_array[i] << " "; // 102
+    // }
+    // cout << endl;
 
-    int k = 10;
-    int arr[k];
-    printf("Array before memset():\n");
-    printArray(arr, k);
-    memset(arr, 10, k * sizeof(arr[0]));
-    printf("\nArray after memset():\n");
-    printArray(arr, k);
-    char str_a[] = "Huuuraaaah!";
-    memset(str_a, 'W', 6);
-    cout << endl;
-    puts(str_a);
-    char str_b[30];
-    strcpy(str_b, "Woooooooooooooooooooo!");
-    puts(str_b);
-    memset(str_b, 'X', 7);
-    puts(str_b);
+    // // realloc example
+    // printf("Input m: ");
+    // int m;
+    // scanf("%i", &m);
+    // char *string_pointer = (char *)malloc(sizeof(int) * m);
+    // cout << "Strlen: " << strlen(string_pointer) << endl;
+    // strcpy(string_pointer, "Keyush");
+    // cout << "Strlen: " << strlen(string_pointer) << endl;
+    // printf("String = %s, address = %p\n", string_pointer, string_pointer);
+    // // reallocating memory
+    // string_pointer = (char *)realloc(string_pointer, sizeof(int) * m * 2);
+    // strcat(string_pointer, ": 'Ruf, ruf!'");
+    // cout << "Strlen: " << strlen(string_pointer) << endl;
+    // printf("String = %s, address = %p\n", string_pointer, string_pointer);
+    // cout << "string_pointer[strlen(string_pointer) - 1] = " << string_pointer[strlen(string_pointer) - 1] << endl;
+    // for (int i = 0; i < 20; i++)
+    // {
+    //     printf("%c", string_pointer[i]);
+    //     if (string_pointer[i] == '\0')
+    //     {
+    //         printf("\nEnd of string!\n");
+    //         printf("%c", string_pointer[i]);
+    //         break;
+    //     }
+    // }
 
-    free(c_array);
-    free(char_array);
-    free(string_pointer);
+    // // examples of using const
+    // const int *const_int;                               // declares that const_int is a variable pointer to a constant integer
+    // int const *const_int_b;                             // is an alternative syntax which does the same
+    // int *const const_pointer = nullptr;                 // declares that const_pointer is constant pointer to a variable integer
+    // int const *const const_int_const_pointer = nullptr; // declares that const_int_const_pointer is constant pointer to a constant integer
+
+    // /*
+    //     Basically ‘const’ applies to whatever is on its immediate left (other than if there is nothing there in which case it applies to whatever is its immediate right).
+    // */
+
+    // int k = 10;
+    // int arr[k];
+    // printf("Array before memset():\n");
+    // printArray(arr, k);
+    // memset(arr, 10, k * sizeof(arr[0]));
+    // printf("\nArray after memset():\n");
+    // printArray(arr, k);
+    // char str_a[] = "Huuuraaaah!";
+    // memset(str_a, 'W', 6);
+    // cout << endl;
+    // puts(str_a);
+    // char str_b[30];
+    // strcpy(str_b, "Woooooooooooooooooooo!");
+    // puts(str_b);
+    // memset(str_b, 'X', 7);
+    // puts(str_b);
+
+    // free(c_array);
+    // free(char_array);
+    // free(string_pointer);
 }
 
 #endif /* part_three_linker_hpp */
